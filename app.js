35a11219abd134bfbcb333a0a56fbfb57fc75368
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 <akhiponnaveni43@gmail.com>
2023-03-28 05:47:11 +0000

    cricket

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { open } = require("sqlite");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+
+const dbpath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+const initilize = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("servering is running http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`db error ${e.message}`);
+    process.exit(1);
+  }
+};
+initilize();
+
+const convertDBObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    SELECT * FROM cricket_team;`;
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDBObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+module.exports = app;
